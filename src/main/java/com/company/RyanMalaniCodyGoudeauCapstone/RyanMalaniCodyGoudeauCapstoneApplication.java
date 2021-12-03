e3c1a8cd80b4d2e16d70025cebf80731a5ad6bd1
ryanmalani <ryanmalani24@gmail.com>
2021-12-02 20:09:45 -0500

    first commit

---
+++ src/main/java/com/company/RyanMalaniCodyGoudeauCapstone/RyanMalaniCodyGoudeauCapstoneApplication.java
@@ -0,0 +1,13 @@
+package com.company.RyanMalaniCodyGoudeauCapstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RyanMalaniCodyGoudeauCapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RyanMalaniCodyGoudeauCapstoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/RyanMalaniCodyGoudeauCapstone/RyanMalaniCodyGoudeauCapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.RyanMalaniCodyGoudeauCapstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RyanMalaniCodyGoudeauCapstoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
