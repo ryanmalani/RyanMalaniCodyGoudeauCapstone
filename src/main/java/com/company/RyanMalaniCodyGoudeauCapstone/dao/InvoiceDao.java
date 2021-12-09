e9a86feb7a636765ed0183192d53517e2c2dab65
Ryan Malani <ryanmalani@MacBook-Pro.fios-router.home>
2021-12-08 23:08:00 -0500

    adding new models/viewmodels

--- src/main/java/com/company/RyanMalaniCodyGoudeauCapstone/dao/InvoiceDao.java
+++ src/main/java/com/company/RyanMalaniCodyGoudeauCapstone/dao/InvoiceDao.java
@@ -1,4 +1,10 @@
 package com.company.RyanMalaniCodyGoudeauCapstone.dao;
 
+import com.company.RyanMalaniCodyGoudeauCapstone.model.Invoice;
+
+import java.util.List;
+
 public interface InvoiceDao {
+
+    Invoice createInvoice(Invoice invoice);
 }
---
+++ src/main/java/com/company/RyanMalaniCodyGoudeauCapstone/viewmodel/Processing_FeeViewModel.java
@@ -0,0 +1,62 @@
+package com.company.RyanMalaniCodyGoudeauCapstone.viewmodel;
+
+import com.company.RyanMalaniCodyGoudeauCapstone.model.Processing_Fee;
+
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class Processing_FeeViewModel {
+
+    // properties
+
+    @NotEmpty(message = "You must provide a value for product type.")
+    @Size(max = 20, message = "You must provide a value less than 20 characters")
+    private String productType;
+    @Digits(2, 2)
+    private BigDecimal fee;
+
+    // getters and setters
+
+    public String getProductType() {
+        return productType;
+    }
+
+    public void setProductType(String productType) {
+        this.productType = productType;
+    }
+
+    public BigDecimal getFee() {
+        return fee;
+    }
+
+    public void setFee(BigDecimal fee) {
+        this.fee = fee;
+    }
+
+    // equals and hashCode
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Processing_Fee that = (Processing_Fee) o;
+        return Objects.equals(productType, that.productType) && Objects.equals(fee, that.fee);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(productType, fee);
+    }
+
+    // toString
+
+    @Override
+    public String toString() {
+        return "Processing_Fee{" +
+                "productType='" + productType + '\'' +
+                ", fee=" + fee +
+                '}';
+    }}
