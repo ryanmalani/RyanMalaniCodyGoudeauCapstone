94efe947a442e7a2813c1049182d7ba2456e01f3
Cody <c.a.goudeau@spartans.nsu.edu>
2021-12-13 23:42:35 -0500

    Invoice/TShirt Controller Completed

--- src/main/java/com/company/RyanMalaniCodyGoudeauCapstone/controller/InvoiceController.java
+++ src/main/java/com/company/RyanMalaniCodyGoudeauCapstone/controller/InvoiceController.java
@@ -1,23 +1,43 @@
 package com.company.RyanMalaniCodyGoudeauCapstone.controller;
 
 import com.company.RyanMalaniCodyGoudeauCapstone.model.Invoice;
+import org.springframework.data.relational.core.sql.In;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/invoice")
 public class InvoiceController {
+
+    private List<Invoice> invoiceList;
+
+    private static int idCounter =1;
+
+    public InvoiceController(){
+        invoiceList = new ArrayList<>();
+
+        invoiceList.add(new Invoice("Cody", "113th Street", "Chicago", "Illinois", "11234", "T_Shirt", 45, "19.99", 1, "19.99", "0.50", "0.50", "20.00", idCounter++));
+        invoiceList.add(new Invoice("Sam", "56th Street", "Salt Lake City", "Utah", "75836", "Console", 50, "499.99", 10, "499.99", "0.40", "0.40", "500.79", idCounter++));
+        invoiceList.add(new Invoice("Robert", "King Road", "Des Moines", "Iowa", "85736", "Game", 14, "59.99", 600, "59.99", "0.40", "0.40", "60.79", idCounter++));
+
+    }
 
     // Create
 
     @PostMapping
+    @RequestMapping(value = "/invoice", method = RequestMethod.POST)
     @ResponseStatus(value = HttpStatus.CREATED)
-    public void createInvoice(@RequestBody @Valid Invoice invoice) {
-        return null;
+    public Invoice createInvoice(@RequestBody @Valid Invoice invoice) {
+
+        invoice.setId(idCounter++);
+        invoiceList.add(invoice);
+
+        return invoice;
     }
 
     // Read
@@ -25,13 +45,42 @@ public class InvoiceController {
     @GetMapping(value = "/{id}")
     @ResponseStatus(value = HttpStatus.OK)
     public Invoice getInvoiceById(@PathVariable int id) {
-        return null;
+
+        Invoice foundInvoice = null;
+
+        for (Invoice invoice : invoiceList) {
+            if (invoice.getId() == id) {
+                foundInvoice = invoice;
+                break;
+            }
+        }
+
+        return foundInvoice;
     }
 
     @GetMapping
     @ResponseStatus(value = HttpStatus.OK)
-    public List<Invoice> getAllInvoices() {
-        return null;
+    public List<Invoice> getAllInvoices(@RequestParam(required = false) String name, @RequestParam(required = false) String total) {
+
+         List<Invoice> returnList = new ArrayList<>();
+
+        if (name != null) {
+            for (Invoice invoice : invoiceList) {
+                if (invoice.getName().contains(name)) {
+                    returnList.add(invoice);
+                }
+            }
+        } else if (total != null) {
+            for (Invoice invoice : invoiceList) {
+                if (invoice.getName().contains(name)){
+                    returnList.add(invoice);
+                }
+            }
+        } else {
+            returnList = invoiceList;
+        }
+
+        return returnList;
     }
 
     // Update
@@ -39,7 +88,19 @@ public class InvoiceController {
     @PutMapping(value = "/{id}")
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
     public void updateInvoice(@PathVariable int id, @RequestBody @Valid Invoice invoice) {
-        return null;
+
+        int index =-1;
+
+        for (int i=0; i < invoiceList.size(); i++) {
+            if (invoiceList.get(i).getId() == id) {
+                index =-1;
+                break;
+            }
+
+            if (index >=0 ) {
+                invoiceList.set(index,invoice);
+            }
+        }
     }
 
     // Delete
@@ -47,6 +108,18 @@ public class InvoiceController {
     @DeleteMapping(value = "/{id}")
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
     public void deleteInvoice(@PathVariable int id) {
-        return null;
+        int index = -1;
+
+        for (int i = 0; i < invoiceList.size(); i++) {
+            if (invoiceList.get(i).getId() == id) {
+                index = i;
+                break;
+            }
+        }
+
+
+        if (index >= 0) {
+            invoiceList.remove(index);
+        }
     }
 }
--- src/main/java/com/company/RyanMalaniCodyGoudeauCapstone/controller/T_ShirtController.java
+++ src/main/java/com/company/RyanMalaniCodyGoudeauCapstone/controller/T_ShirtController.java
@@ -5,18 +5,37 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/t_shirts")
 public class T_ShirtController {
 
+    private List<T_Shirt> t_shirtList;
+
+    private static int idCounter =1;
+
+    public T_ShirtController(){
+        t_shirtList = new ArrayList<>();
+
+        t_shirtList.add(new T_Shirt("Medium", "Red", "Limited edition baseball TShirt", "49.99", 700, idCounter++));
+        t_shirtList.add(new T_Shirt("Large", "Blue", "Limited edition basketball TShirt", "39.99", 670, idCounter++));
+        t_shirtList.add(new T_Shirt("Small", "Black", "Off White TShirt", "80.00", 560, idCounter++));
+
+
+    }
+
     // Create
 
     @PostMapping
     @ResponseStatus(value = HttpStatus.CREATED)
     public T_Shirt createT_Shirt(@RequestBody @Valid T_Shirt t_shirt) {
-        return null;
+
+        t_shirt.setId(idCounter++);
+        t_shirtList.add(t_shirt);
+
+        return t_shirt;
     }
 
     // Read
@@ -24,25 +43,75 @@ public class T_ShirtController {
     @GetMapping(value = "/{id}")
     @ResponseStatus(value = HttpStatus.OK)
     public T_Shirt getT_ShirtById(@PathVariable int id) {
-        return null;
+
+        T_Shirt foundT_Shirt = null;
+
+        for (T_Shirt t_shirt : t_shirtList) {
+            if (t_shirt.getId() == id) {
+                foundT_Shirt = t_shirt;
+                break;
+            }
+        }
+
+        return foundT_Shirt;
+
     }
 
     @GetMapping
     @ResponseStatus(value = HttpStatus.OK)
-    public List<T_Shirt> getAllT_Shirts() {
-        return null;
+    public List<T_Shirt> getAllT_Shirts(@RequestParam(required = false) String size, @RequestParam(required = false) String quantity) {
+
+        List<T_Shirt> returnList = new ArrayList<>();
+
+        if (size != null) {
+            for (T_Shirt t_shirt : t_shirtList) {
+                if (t_shirt.getSize().contains(size)) {
+                    returnList.add(t_shirt);
+                }
+            }
+        } else if (quantity != null) {
+            for (T_Shirt t_shirt : t_shirtList) {
+                if (t_shirt.getSize().contains(size)) {
+                    returnList.add(t_shirt);
+                }
+            }
+        } else {
+            returnList = t_shirtList;
+        }
+
+        return returnList;
     }
 
     @GetMapping(value = "/{color}")
     @ResponseStatus(value = HttpStatus.OK)
-    public List<T_Shirt> getT_ShirtsByColor(@PathVariable String color) {
-        return null;
+    public T_Shirt getT_ShirtsByColor(@PathVariable String color) {
+
+        T_Shirt foundT_Shirt = null;
+
+        for (T_Shirt t_shirt : t_shirtList) {
+            if (t_shirt.getColor() == color) {
+                foundT_Shirt = t_shirt;
+                break;
+            }
+        }
+
+        return foundT_Shirt;
     }
 
     @GetMapping(value = "/{size}")
     @ResponseStatus(value = HttpStatus.OK)
-    public List<T_Shirt> getT_ShirtsBySize(@PathVariable String size) {
-        return null;
+    public T_Shirt getT_ShirtsBySize(@PathVariable String size) {
+
+        T_Shirt foundT_Shirt = null;
+
+        for (T_Shirt t_shirt : t_shirtList) {
+            if (t_shirt.getSize() == size) {
+                foundT_Shirt = t_shirt;
+                break;
+            }
+        }
+
+        return foundT_Shirt;
     }
 
     // Update
@@ -50,7 +119,19 @@ public class T_ShirtController {
     @PutMapping(value = "/{id}")
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
     public void updateT_Shirt(@PathVariable int id, @RequestBody @Valid T_Shirt t_shirt) {
-        return null;
+
+        int index =-1;
+
+        for (int i=0; i < t_shirtList.size(); i++) {
+            if (t_shirtList.get(i).getId() == id) {
+                index = i;
+                break;
+            }
+
+            if (index >=0 ) {
+                t_shirtList.set(index,t_shirt);
+            }
+        }
     }
 
     // Delete
@@ -58,6 +139,18 @@ public class T_ShirtController {
     @DeleteMapping(value = "/{id}")
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
     public void deleteT_Shirt(@PathVariable int id) {
-        return null;
+
+        int index =-1;
+
+        for (int i = 0; i < t_shirtList.size(); i++) {
+            if (t_shirtList.get(i).getId() == id) {
+                index = i;
+                break;
+            }
+        }
+
+        if (index >= 0) {
+            t_shirtList.remove(index);
+        }
     }
 }
