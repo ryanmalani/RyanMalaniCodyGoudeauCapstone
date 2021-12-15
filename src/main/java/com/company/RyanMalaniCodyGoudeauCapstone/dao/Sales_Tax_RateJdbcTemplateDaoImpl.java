667faaff4ad5a798b1cecd4df92abd1db0b86cb3
ryanmalani <ryanmalani24@gmail.com>
2021-12-14 21:58:39 -0500

    fixed sales tax rate impl

--- src/main/java/com/company/RyanMalaniCodyGoudeauCapstone/dao/Sales_Tax_RateJdbcTemplateDaoImpl.java
+++ src/main/java/com/company/RyanMalaniCodyGoudeauCapstone/dao/Sales_Tax_RateJdbcTemplateDaoImpl.java
@@ -1,8 +1,17 @@
 package com.company.RyanMalaniCodyGoudeauCapstone.dao;
 
 import com.company.RyanMalaniCodyGoudeauCapstone.model.Sales_Tax_Rate;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class Sales_Tax_RateJdbcTemplateDaoImpl implements Sales_Tax_RateDao {
+
+    private JdbcTemplate jdbcTemplate;
+
+    public Sales_Tax_RateJdbcTemplateDaoImpl() {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     @Override
     public Sales_Tax_Rate getSales_Tax_Rate(String state) {
