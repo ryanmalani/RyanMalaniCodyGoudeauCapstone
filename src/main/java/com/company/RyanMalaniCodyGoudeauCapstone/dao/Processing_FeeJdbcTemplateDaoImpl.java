bb4606924e9292ab61f86e2848a623cfdc1545de
Ryan Malani <ryanmalani@10-3-15-249.dhcp.marymount.edu>
2021-12-16 12:42:44 -0500

    processing fee dao impl completed

--- src/main/java/com/company/RyanMalaniCodyGoudeauCapstone/dao/Processing_FeeJdbcTemplateDaoImpl.java
+++ src/main/java/com/company/RyanMalaniCodyGoudeauCapstone/dao/Processing_FeeJdbcTemplateDaoImpl.java
@@ -2,11 +2,20 @@ package com.company.RyanMalaniCodyGoudeauCapstone.dao;
 
 import com.company.RyanMalaniCodyGoudeauCapstone.model.Processing_Fee;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 @Repository
 public class Processing_FeeJdbcTemplateDaoImpl implements Processing_FeeDao {
+
+    // prepared statement
+
+    private static final String SELECT_PROCESSING_FEE_SQL =
+            "select * from processing_fee where product_type = ?";
 
     private JdbcTemplate jdbcTemplate;
 
@@ -17,6 +26,16 @@ public class Processing_FeeJdbcTemplateDaoImpl implements Processing_FeeDao {
 
     @Override
     public Processing_Fee getProcessing_Fee(String product_type) {
-        return null;
+        try {
+            return jdbcTemplate.queryForObject(SELECT_PROCESSING_FEE_SQL, this::mapRowToProcessingFee, product_type);
+        } catch (EmptyResultDataAccessException e) {
+            throw new IllegalArgumentException("Product type must be t_shirt, console, or game")
+        }
+    }
+
+    public Processing_Fee mapRowToProcessingFee(ResultSet resultSet, int rowNum) throws SQLException {
+        return new Processing_Fee(
+                resultSet.getString("product_type"),
+                resultSet.getBigDecimal("fee"));
     }
 }
