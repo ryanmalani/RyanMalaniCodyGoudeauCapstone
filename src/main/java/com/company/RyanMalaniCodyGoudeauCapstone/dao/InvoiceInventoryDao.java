7c1a49ad987a128da076796c89b31406d60048f3
ryanmalani <ryanmalani24@gmail.com>
2021-12-16 11:15:41 -0500

    removing unnecessary methods from invoicedao

--- src/main/java/com/company/RyanMalaniCodyGoudeauCapstone/dao/InvoiceInventoryDao.java
+++ src/main/java/com/company/RyanMalaniCodyGoudeauCapstone/dao/InvoiceInventoryDao.java
@@ -2,13 +2,7 @@ package com.company.RyanMalaniCodyGoudeauCapstone.dao;
 
 import com.company.RyanMalaniCodyGoudeauCapstone.model.Invoice;
 
-import java.util.List;
-
 public interface InvoiceInventoryDao {
 
-    Invoice getInvoice(int id);
-    List<Invoice> getAllInvoices();
     Invoice addInvoice(Invoice invoice);
-    void updateInvoice(Invoice invoice);
-    void deleteInvoice(int id);
 }
